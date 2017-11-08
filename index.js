136eb7c74ae5925fa124ccb73ed7cef6d5a61637
KrBaIO3 <krbaio3@gmail.com>
2017-11-08 20:58:00 +0100

    feat(addon)

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const buffer = Buffer.alloc(8);
-console.log(buffer);
+const addon = require('addon');
 
-const buffer2 = Buffer.allocUnsafe(8);
-console.log(buffer2);
-
-const string = 'avi\'on';
-// const buffer3 = Buffer.from(string);
-// console.log(buffer3.toString());
-
-const buffer3 = Buffer.from(string, 'ascii');
-console.log(string, string.toString(), string.length);
-console.log(buffer3, buffer3.toString(), buffer3.length);
+console.log(addon.hello());
