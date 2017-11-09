e10a2f146df601050bed765c313fd494aadf1669
KrBaIO3 <krbaio3@gmail.com>
2017-11-09 18:42:40 +0100

    feat(bloqueo llamada)
    bloquea las IO mientras trabaja

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const m = require('./mathfun');
+const slowAdd = (a, b) => {
+  for (let index = 0; index < 999999999; index++) {}
+  return a + b;
+}
 
-const processResult = (error, result, time) => {
-  if (error) {
-    console.log(`ERROR: ${error.message}`);
-  }else{
-console.log(`The result is: ${result} (${time} ms)`)
-  }
-};
-[5,25,4,8,64].forEach(value => m.intSqrt(value, processResult));
+const a= slowAdd (6,9);
+const b = slowAdd (1,42);
+const c = slowAdd (23,42);
+const d = slowAdd (5,9);
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
