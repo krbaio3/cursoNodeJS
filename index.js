a0d569759a82280d793bc84b50a02159c84c489c
KrBaIO3 <krbaio3@gmail.com>
2017-11-21 20:33:55 +0100

    feat(ejercicio 5 hibrido)

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const fs = require('fs');
 
-const readFileAsArray = (file) => {
+const readFileAsArray = (file, cb = () => {}) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
       if (err) {
+        reject(error)
         return cb(err);
       }
 
       const lines = data.toString().trim().split('\n');
       resolve(lines);
+      cb(null, lines);
     });
   })
 };
 
-readFileAsArray('./numbers')
-  .then((lines) => {
-    const number = lines.map(Number);
-    const oddNumber = number.filter((number) => number % 2 === 1);
-    console.log(`Odd numbers count: ${oddNumber.length}`)
-  })
-  .catch((error) => {
-    throw error;
-  });
+readFileAsArray('./numbers', (error, lines) => {
+if (error) {
+  throw error;
+}
+
+const number = lines.map(Number);
+const oddNumber = number.filter((number) => number % 2 === 1);
+console.log(`Odd numbers count: ${oddNumber.length}`);
+})
+
+// .then((lines) => {
+//     const number = lines.map(Number);
+//     const oddNumber = number.filter((number) => number % 2 === 1);
+//     console.log(`Odd numbers count: ${oddNumber.length}`)
+//   })
+//   .catch((error) => {
+//     throw error;
+//   });
