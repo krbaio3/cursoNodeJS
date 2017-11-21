02e2dbdcd4456d4ef8478ba6ea2316f168c16a78
KrBaIO3 <krbaio3@gmail.com>
2017-11-21 21:04:45 +0100

    feat(emitter)

--- comand.cmd
+++
@@ -1,6 +0,0 @@
-echo instalar node-gyp de manera global
-cd addon-src
-node-gyp configure
-node-gyp build
-mkdir node_modules
-cp build/Release/addon.node ../node_modules
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const {
-  countOddPromise,
-  countOddCallback,
-  countOddAsync,
-} = require ('./countOdd');
+const EventEmitter = require('events').EventEmitter;
 
-countOddCallback('./numbers');
-countOddPromise('./numbers');
-countOddAsync('./numbers');
+const deferredProcess = (triesCount, emitter) => {
+  process.nextTick(() => {
+    let count = 0;
+    emitter.emit('superNabo');
+
+    const interval = setInterval(() => {
+      emitter.emit('data', ++count);
+      if (count === triesCount) {
+        emitter.emit('end', count);
+        clearInterval(interval);
+      }
+    }, 300);
+  });
+}
+
+const dataRetriever = (triesCount) => {
+  const emitter = new EventEmitter();
+  deferredProcess(triesCount, emitter);
+  return emitter;
+}
+
+
+const dataRetrieverHandler = dataRetriever(10);
+
+dataRetrieverHandler.on('start',() => console.log('started'));
+dataRetrieverHandler.on('data',(data) => console.log(`data: ${data}`));
+dataRetrieverHandler.on('end',(data) => console.log(`end: ${data}`));
