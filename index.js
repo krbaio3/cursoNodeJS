e956297edd150993ea8f62842e1c6c5f884f3845
KrBaIO3 <krbaio3@gmail.com>
2017-11-22 20:34:01 +0100

    feat(server)

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-const http = require('http');
-let count = 0;
-const req = http.request({
-  hostname: 'www.elmundo.com'
-}, (res) => {
-  // console.log(`Status code: ${res.statusCode}`);
-  // console.log(res.headers);
-  res.on('data', (data) => {
-    count += 1;
-    // console.log(data.toString());
-    console.log(count)
-  });
+const server = require('http').createServer();
+const fs = require('fs');
+
+const routes = {
+  '/home': (res, fs) => {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.end(fs.readFileSync('./index.html'));
+  },
+
+  '/': (res) => {
+    res.writeHead(301, {
+      'Location': '/home'
+    });
+    res.end();
+  },
+  '/api': (res) => {
+    res.writeHead(200, {
+      'Content-Type': 'aplication/json'
+    });
+    const data = { company: 'Lemoncode'};
+    res.end(JSON.stringify(data));
+  }
+};
+
+const notFoundResponse = (res) => {
+  res.writeHead(404);
+  res.end();
+};
+
+const routeHandler = (route) => (!!routes[route] ?
+  routes[route] : notFoundResponse
+);
+
+server.on('request', (req, res) => {
+  const route = routeHandler(req.url);
+  route(res, fs);
 });
 
-req.on('error', (error) => {
-  console.error(error);
-});
-
-// console.log(req.agent);
-
-req.end();
+server.listen(3000);
