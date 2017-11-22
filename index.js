ff6d0615594841ffeed5ec91504fea98d8a78de4
KrBaIO3 <krbaio3@gmail.com>
2017-11-22 19:48:25 +0100

    feat(server)
    varios servers

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ server.on('request', (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   });
-  res.end('Hello word\n');
+  res.write('Hello word\n');
+
+  setTimeout(() => {
+    res.write('Another hello\n');
+  }, 1000);
+
+  setTimeout(() => {
+    res.write('super nabo\n');
+  },2000);
 });
 
 server.listen(3000);
