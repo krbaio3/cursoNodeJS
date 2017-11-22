0b28ff69186fd7ecb0fbb2154050b9765028c2e2
KrBaIO3 <krbaio3@gmail.com>
2017-11-22 19:36:15 +0100

    feat(server)

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-const EventEmitter = require('events').EventEmitter;
-const fs = require('fs');
+const server = require('http').createServer();
 
-class WithTime extends EventEmitter {
+server.on('request', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Hello word\n');
+});
 
-  execute(asyncFun, ...args) {
-    console.time('Executed in');
-    asyncFun(...args, (error, data) => {
-      if (error) {
-        this.emit('error', error);
-        return console.timeEnd('Executed in');
-      }
-
-      this.emit('data', data);
-      console.timeEnd('Executed in');
-      this.emit('end');
-    });
-  }
-}
-
-const withTime = new WithTime();
-
-withTime.on('data', (data) => console.log(data));
-withTime.on('error', (error) => console.error(error));
-withTime.on('end', () => console.log('end'));
-
-withTime.execute(fs.readFile, __filename);
+server.listen(3000);
